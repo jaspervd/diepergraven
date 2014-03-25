0f38f993e024ad848cbf9bee77427159123c2b95
Jasper Van Damme <jasper.van.damme@student.howest.be>
2014-03-25 13:23:38 +0100

    fixed paths

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 
 define('WWW_ROOT', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
-require_once WWW_ROOT . 'dao' . DIRECTORY_SEPARATOR . 'ScoresDAO.php';
+require_once WWW_ROOT . 'api' . DIRECTORY_SEPARATOR . 'dao' . DIRECTORY_SEPARATOR . 'ScoresDAO.php';
 require_once WWW_ROOT . 'api' . DIRECTORY_SEPARATOR . 'Slim' . DIRECTORY_SEPARATOR . 'Slim.php';
 
 \Slim\Slim::registerAutoloader();
